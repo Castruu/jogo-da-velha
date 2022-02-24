2fa7fa346bc83d72f46c23182f2a8b3d761ea64d
Castruu <vitorfycastroo@gmail.com>
2022-02-24 02:11:43 -0300

    All Files

---
+++ scripts/script.js
@@ -0,0 +1,81 @@
+let index = 0;
+
+let boxes = document.querySelectorAll(".box");
+
+let winning = [
+    [
+        0, 1, 2
+    ],
+    [
+        3, 4, 5
+    ],
+    [
+        6, 7, 8
+    ],
+    [
+        0, 3, 6
+    ],
+    [
+        1, 4, 7
+    ],
+    [
+        2, 5, 8
+    ],
+    [
+        0, 4, 8
+    ],
+    [
+        2, 4, 6
+    ]
+]
+
+function loadPage() {
+    for (box of boxes) {
+        box.onclick = draw;
+    }
+}
+
+
+function verify() {
+    for (arr of winning) {
+        let colors = [];
+        for (i of arr) {
+            box = boxes[i]
+            let backgroundColor = box.style.backgroundColor;
+            if (!backgroundColor) continue;
+            colors.push(backgroundColor);
+        }
+        console.log(colors);
+        console.log(arr);
+        for (color in colors) {
+            if (color == 0) continue;
+            console.log(color)
+            if (colors[color] == colors[color - 1]) {
+                console.log(colors[color], colors[color - 1]);
+                if (color == 2) {
+                    alert("Won");
+                    document.location.reload();
+                    return;
+                }
+            } else {
+                console.log("bla");
+                break;
+            }
+        }
+    }
+}
+
+function draw() {
+    if (this.style.backgroundColor) {
+        return;
+    }
+    if (index == 0) {
+        this.style.backgroundColor = "black"
+        index = 1;
+    } else {
+
+        this.style.backgroundColor = "blue"
+        index = 0;
+    }
+    verify();
+}
